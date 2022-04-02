91a758400a978c283f762e435050a9e63a03c14c
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-04-02 19:50:01 -0300

    feat: validador estruturado caso input seja required

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,49 @@
+let form = document.querySelector('.b7validator');
+
 let b7Validador = {
   handleSubmit: event => {
     event.preventDefault(); // previne o comportamento padrão
+    let send = true;
+
+    let inputs = form.querySelectorAll('input');
+
+    for (let i = 0; i < inputs.length; i++) {
+      let input = inputs[i];
+      let check = b7Validador.checkInput(input);
+      if (check !== true) {
+        send = false;
+        //exibir o erro
+        console.log(check);
+      }
+    }
+
+    if (send) {
+      form.submit();
+    }
+  },
+  checkInput: input => {
+    let rules = input.getAttribute('data-rules');
+
+    if (rules !== null) {
+      rules = rules.split('|');
+
+      for (let item in rules) {
+        let rulesDetails = rules[item].split('=');
+
+        switch (rulesDetails[0]) {
+          case 'required':
+            if (input.value === '') {
+              return 'Campo não pode ser vazio.';
+            }
+            break;
+          case 'min':
+            break;
+        }
+      }
+    }
+
+    return true;
   }
 };
-let form = document.querySelector('.b7validator');
 
 form.addEventListener('submit', b7Validador.handleSubmit);

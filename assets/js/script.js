0888a976ca36b71d462744abaa9a9608d989ca24
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-04-03 19:19:58 -0300

    inclui validação do campo email

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,15 @@ let b7Validador = {
               );
             }
             break;
+          case 'email':
+            if (input.value !== '') {
+              let regex =
+                /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+              if (!regex.test(input.value.toLowerCase())) {
+                return 'E-mail digitado não é válido!';
+              }
+            }
+            break;
         }
       }
     }
